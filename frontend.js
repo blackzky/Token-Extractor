69d91cf6c216e264cf6ea6bccf1918041f1c81f7
Gervel Giva <gervelgiva@gmail.com>
2017-08-10 17:46:09 +0800

    Signle Download UI

--- frontend.js
+++ frontend.js
@@ -1,50 +1,23 @@
 /* jshint esversion: 6 */
 
-const UI = `
-<div id="filterManager" style="position: fixed; top:1em; left: 1em; border: 1px solid black">
-    <div>
-        Show All: <input type="checkbox" id="toggleAssetsView" />
-    </div>
-
-    <div id="downloadSettings">
-        <label for="assetName">Name:</label> <span id="assetName"></span>
-        <br />
-        <span>Tags:</span>
-        <br />
-        <ul id="assetTags"></ul>
-        <br />
-        <select id="newTagSelect" name="newTagSelect">
-            <option value="" selected disabled>Select a tag</option>
-        </select>
-        <br />
-        <input type="text" placeholder="Custom tag" id="customTag" />
-        <button id="addCustomTag">Add</button>
-        <br />
-        <button id="downloadAsset">Download</button>
-    </div>
+const EXTRACTOR_UI = `
+<div>
+    <a href="#" class="download">Download</a>
+    <span class='downloadStatus'><span>
 </div>
 `;
-
-const DOWNLOAD_BUTTON = `<a href="#" id="download">Download</a>`;
 
 $(() => {
     'use strict';
 
-    const ASSETS_LIST_KEY = 'AssetList';
+    const SERVER_URL = 'http://localhost:3000/extract';
     let ASSETS_DOM = $(".marketplacelistingitem");
-    let ASSETS = {};
 
-
-    // [1] Load UI
     if (AddScript()) {
         LoadUI();
-
-        // [2] Add event for "Show all" and 'Download'
-        $('body').on('change', '#toggleAssetsView', ToggleAssetList);
-        $('body').on('click', '#download', DownloadToken);
-
-        // [3] Load Asset Data from local storage
-        LoadAssetData();
+        $('body').on('click', '.download', DownloadToken);
+    } else {
+        alert('Failed to load extractor');
     }
 
     //////////////////////////////
@@ -54,40 +27,19 @@ $(() => {
     }
 
     function LoadUI() {
-        $('body').append(UI);
-        $('#downloadSettings').hide();
-
         ASSETS_DOM.each((i, e) => {
-            $(e).prepend(DOWNLOAD_BUTTON);
+            $(e).prepend(EXTRACTOR_UI);
         });
-    }
-
-    // TODO: Implement
-    function ToggleAssetList(e) {
-        let checked = e.target.checked;
-        console.log('checked:', checked);
-    }
-
-    function LoadAssetData() {
-        let assets = JSON.parse(localStorage.getItem(ASSETS_LIST_KEY));
-        if (typeof assets === 'object') {
-            console.log('assset', assets);
-            ASSETS = assets;
-            // [4] Iterate list and hide assets that are tagged as hidden
-        } else {
-            throw 'Invalid data from local storage';
-        }
     }
 
     function DownloadToken(e) {
         e.preventDefault();
-        const EXTRACT_URL = 'http://localhost:3000/extract';
 
-        let parentDOM = $(e.currentTarget).parent();
-        const tokenPack = $("h1.setname").text().trim();
-        const tokenName = parentDOM.find('div.desc em').text().trim();
-        const imageUrl = parentDOM.find("div.inneritem a.lightly")[0].href;
-        const tokenTags = parentDOM.attr('original-title');
+        let parentDOM = $(e.currentTarget).parent().parent();
+        let tokenPack = $("h1.setname").text().trim();
+        let tokenName = parentDOM.find('div.desc em').text().trim();
+        let imageUrl = parentDOM.find("div.inneritem a.lightly")[0].href;
+        let tokenTags = parentDOM.attr('original-title');
 
         let payload = {
             'tokenPack': tokenPack,
@@ -95,29 +47,31 @@ $(() => {
             'imageUrl': imageUrl,
             'tokenTags': tokenTags
         };
-        $.post(EXTRACT_URL, payload).done((data) => {
-            alert("File downloaded");
+
+        parentDOM.find('span.downloadStatus').text('Downloading...');
+        parentDOM.find('a.download').hide();
+
+        $.post(SERVER_URL, payload).done((data) => {
+            parentDOM.find('span.downloadStatus').text('Downloaded');
         });
-        // $('#downloadSettings').show();
     }
 
 });
 
-
 /*
 [TOKEN]
 	$("h1.setname").text().trim();
-		Devin Token Pack 45 - Heroic Characters 1
+		=> ex. "Devin Token Pack 45 - Heroic Characters 1"
 
 [TOKEN NAME]
 	$($("div.desc em")[0]).text().trim();
-        Dwarf_Male_Fighter2
+        => ex. "Dwarf_Male_Fighter2"
         
 [IMAGE URI]
 	$("div.inneritem a.lightly")[0].href
-		https://s3.amazonaws.com/files.d20.io/marketplace/24475/T-WjDkHpIdZUQuZUB8YPHw/max.png?1375971198
+		=> ex. "https://s3.amazonaws.com/files.d20.io/marketplace/24475/T-WjDkHpIdZUQuZUB8YPHw/max.png?1375971198"
 		
 [Tags]
 	$($("div.marketplacelistingitem")[0]).attr('original-title');
-		"shield axe dwarf token NPC PC character fantasy token fantasy"
+		=> ex. "shield axe dwarf token NPC PC character fantasy token fantasy"
 */
